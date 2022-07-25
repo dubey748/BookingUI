129f9d1e0b8b09826f530a5d852a5c0743db5b53
ABhishek Dubey <65311526+dubey748@users.noreply.github.com>
2022-07-25 19:40:20 +0530

    Add files via upload

---
+++ src/components/searchItem/SearchItem.jsx
@@ -0,0 +1,41 @@
+import "./searchItem.css";
+
+const SearchItem = () => {
+  return (
+    <div className="searchItem">
+      <img
+        src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1"
+        alt=""
+        className="siImg"
+      />
+      <div className="siDesc">
+        <h1 className="siTitle">The Taj</h1>
+        <span className="siDistance">500m from Station</span>
+        <span className="siTaxiOp">Free airport taxi</span>
+        <span className="siSubtitle">
+         Air conditioning  Apartment with swimming pool
+        </span>
+        <span className="siFeatures">
+          Entire studio • 1 bathroom • 21m² 1 full bed
+        </span>
+        <span className="siCancelOp">Free cancellation </span>
+        <span className="siCancelOpSubtitle">
+          You can cancel later, so lock in this great price today!
+        </span>
+      </div>
+      <div className="siDetails">
+        <div className="siRating">
+          <span>Excellent</span>
+          <button>8.9</button>
+        </div>
+        <div className="siDetailTexts">
+          <span className="siPrice">₹ 420000</span>
+          <span className="siTaxOp">Includes taxes and fees</span>
+          <button className="siCheckButton">See availability</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchItem;
